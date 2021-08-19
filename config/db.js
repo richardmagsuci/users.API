25b4df8688a150a46d5c0a66e6cd267db4d79361
Richard Magsuci <41479802+richardmagsuci@users.noreply.github.com>
2021-08-19 23:39:00 +0800

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url : "mongodb://rmagsuci:nimda1234@ds139632.mlab.com:39632/user_profile_rvm"
+	url : "mongodb://rmagsuci:Magsuci1227@cluster0-shard-00-00.5xgtn.mongodb.net:27017,cluster0-shard-00-01.5xgtn.mongodb.net:27017,cluster0-shard-00-02.5xgtn.mongodb.net:27017/fsd?ssl=true&replicaSet=atlas-hw1nmw-shard-0&authSource=admin&retryWrites=true&w=majority"
 }
